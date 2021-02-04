76d0d7acf7342cb28413a1e98bf4de15d3163552
woodylucas <wlucas0311@gmail.com>
2021-02-03 19:22:46 -0500

    linked list data structure

---
+++ datas_structures/linkedlist.js
@@ -0,0 +1,97 @@
+/* 
+
+A Linked List is an ordered collection of data, the collection contains an ordered number of nodes.
+
+Node contains some amount of data, and reference to the next node. 
+
+
+Chain of nodes: ordered that is always maintained 
+
+
+2 special Nodes: Head(first) & Tail(last)
+
+
+
+
+
+*/
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  // Insert first node
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+    this.size++;
+  }
+
+  // Insert last node
+  insertLast(data) {
+    let node = new Node(data);
+    let current;
+    if (!this.head) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.size++;
+  }
+
+  // Insert at index
+  insertAt(data, index) {
+    if (index > 0 && index > this.size) {
+      return;
+    }
+
+    // If first index
+    if (index === 0) {
+      this.insertFirst(data);
+      return;
+    }
+
+    const node = new Node(data);
+    let current, previous;
+
+    // Set current to first
+    current = this.head;
+    let count = 0;
+
+    while (count < index) {
+      previous = current;
+      count++;
+      current = current.next;
+    }
+    node.next = current;
+    previous.next = node;
+    this.size++;
+  }
+
+  // Get at index
+
+  // Remove at index
+
+  // Clear list
+
+  // Print list data
+  printListData() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
