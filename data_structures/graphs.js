e2d9c09587b74128030ad4ca3b59400dab066673
woodylucas <wlucas0311@gmail.com>
2021-03-05 21:50:23 -0500

    implemented dfs, bfs

--- data_structures/graphs.js
+++ data_structures/graphs.js
@@ -1,26 +1,80 @@
+function destinations(airlines, routes) {
+  const graph = new Graph();
+  airports.forEach((airport) => graph.addNode(airport));
+  routes.forEach((route) => graph.addEdge(...route));
+  console.log(graph.dfs("PHX"));
+}
+
 class Graph {
   constructor() {
-    this.adjacencyList = {};
+    this.adjacenyList = new Map();
   }
+  // Vertex is our node: airport
+  addNode(airport) {
+    this.adjacenyList.set(airport, []);
+  }
+  // Add Edge, undirected
+  // Direct flights between two airports
+  addEdge(destination, origin) {
+    // We represent the relationship with the edge with an array with the node that has a node within it
+    this.adjacenyList.get(destination).push(origin);
+    this.adjacenyList.get(origin).push(destination);
+  }
+  // FIRST IN FIRST OUT  O(v + e)
+  bfs(start) {
+    const visited = new Set();
+    const queue = [start];
+    while (queue.length > 0) {
+      const airport = queue.shift(); // mutates the queue
+      const destinations = this.adjacenyList.get(airport); // return an array
 
-  addVertex(vertex) {
-    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
-  }
+      for (const destination of destinations) {
+        if (destination === "BKK") {
+          console.log(destination, "found it!");
+        }
 
-  addEdge(vertex1, vertex2) {
-    this.adjacencyList[vertex1].push(vertex2);
-    this.adjacencyList[vertex2].push(vertex1);
+        if (!visited.has(destination)) {
+          visited.add(destination);
+          queue.push(destination);
+          console.log(destination);
+        }
+      }
+    }
   }
-  removeEdge(vertex1, vertex2) {
-    this.adjacencyList[vertex1].filter((vertex) => vertex != vertex2);
-    this.adjacencyList[vertex2].filter((vertex) => vertex != vertex1);
+  // O(v + e) LAST IN FIRST OUT
+  dfs(start, visited = new Set()) {
+    console.log(start);
+
+    visited.add(start);
+
+    const destinations = this.adjacenyList.get(start);
+
+    for (const destination of destinations) {
+      // Base case
+      if (destination === "BKK") {
+        console.log("DFS found Bangkok in steps");
+        return;
+      }
+      if (!visited.has(destination)) {
+        this.dfs(destination, visited);
+      }
+    }
   }
 }
 
-const g = new Graph();
+const airports = "PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM".split(" ");
 
-g.addVertex("Tokyo");
-g.addVertex("China");
-g.addVertex("Dallas");
-g.addVertex("Aspen");
-console.log(g);
+const routes = [
+  ["PHX", "LAX"],
+  ["PHX", "JFK"],
+  ["JFK", "OKC"],
+  ["JFK", "HEL"],
+  ["JFK", "LOS"],
+  ["MEX", "LAX"],
+  ["MEX", "BKK"],
+  ["MEX", "LIM"],
+  ["MEX", "EZE"],
+  ["LIM", "BKK"],
+];
+
+console.log(destinations(airports, routes));
